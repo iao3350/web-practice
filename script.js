e099026bc20b8b05c2ef60d9fc9fda73012eab9b
iao3350 <GitHubiao3350@gmail.com.>
2026-08-23 21:44:00 +0900

    JavaScriptで曜日ごとの時間表示を追加

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
 console.log("JavaScriptが読み込まれました！");
 
-let count = 1;
-count = 2;
+const daySelect = document.getElementById("day");
+
+console.log(daySelect);
+
+const timeSelect = document.getElementById("time");
+
+const lessonTimes = {
+    monday: ["19:00", "19:30", "20:00"],
+    tuesday: ["18:30", "19:00", "19:30", "20:00"],
+};
+
+daySelect.addEventListener("change", function(event) {
+
+    const times = lessonTimes[event.target.value];
+
+    console.log(lessonTimes[event.target.value]);
+
+    timeSelect.innerHTML = "";
+
+    times.forEach(function(time) {
+        const option = document.createElement("option");
+        option.textContent = time;
+        timeSelect.appendChild(option);
+    });
+});
